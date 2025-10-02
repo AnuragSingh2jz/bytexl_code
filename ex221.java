ea4c02187ebf72dc55204f55feea5eb9b63930e9
Anurag singh <mibanurag9140@gmail.com>
2025-10-02 23:02:10 +0530

    exp 2.2.1

---
+++ ex221.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex221 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> nums = new ArrayList<>();
+        int sum=0;
+        System.out.println("If u want to stop the insertion of element type````` done`````");
+        while(true){
+            String input = sc.nextLine();
+            if(input.equals("done")){
+                break;
+            }
+            try{
+                int n = Integer.parseInt(input);
+                nums.add(n);
+
+
+
+            }catch(NumberFormatException e){
+                System.out.println("invalid input");
+            }
+
+        }
+
+        for(Integer x: nums){
+            sum+=x;
+        }
+
+        System.out.println("the sum of elements is arrayList is : "+sum);
+        sc.close();
+    }
+}
